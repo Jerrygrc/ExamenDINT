06663b6856951cd3e2a0bc2cc6b3956f4d83f6e5
Gerardo Ramallo Couce <gerardoramallocouce@Mac-mini-de-Gerardo.local>
2025-03-11 19:00:51 +0100

    final

--- Articulos/View/ProductListView.swift
+++ Articulos/View/ProductListView.swift
@@ -29,7 +29,7 @@ struct ProductListView: View {
                         }
                     }
                 }
-            }
+            }.navigationTitle("Productos")
         }.toolbar{
             ToolbarItemGroup(placement: .bottomBar){
                 Button(action: {
